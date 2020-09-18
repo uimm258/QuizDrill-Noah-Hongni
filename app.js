d2c7bdf73d4d9f34282d56261d8cadcdb89ce85d
uimm258 <uimm258@gmail.com>
2020-09-18 02:03:41 -0700

    more modification

--- app.js
+++ app.js
@@ -6,25 +6,106 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'When was the online game "League of Legends" released?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        '2008',
+        '2009',
+        '2010',
+        '2011'
       ],
-      correctAnswer: 'green'
+      correctAnswer: '2009'
     },
     {
-      question: 'What is the current year?',
+      question: 'Which video game company developed the game "League of Legends?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Activison',
+        'Capcom',
+        'Riot',
+        'Blizzard'
       ],
-      correctAnswer: '2019'
-    }
+      correctAnswer: 'Roit'
+    },
+    {
+      question: 'Who is the champion Tryndamere married to in "League of Legends?"',
+      answers: [
+        'Ashe',
+        'Silver',
+        'Lux',
+        'Janna'
+      ],
+      correctAnswer: 'Ashe'
+    },
+    {
+      question: 'How many lanes are there in the rank map of the game "League of Legends?"',
+      answers: [
+        'One',
+        'Two',
+        'Three',
+        'The map is a circle'
+      ],
+      correctAnswer: 'Three'
+    },
+    {
+      question: 'What animals are seen in the Howling Abyss map of the game "League of Legends?"',
+      answers: [
+        'poro',
+        'unicorn',
+        'mermaid',
+        'There is no animals in this map'
+      ],
+      correctAnswer: 'poro'
+    },
+    {
+      question: 'Which tribe do the "League of Legends" champions Anivia, Braum, Gragas, Nunu and Tryndamere belong to?',
+      answers: [
+        'Winter Claw',
+        'Frost Guard',
+        'Avarosan',
+        'All of them'
+      ],
+      correctAnswer: 'Avarosan'
+    },
+    {
+      question: 'Who joins Serylda and Avarosa as the third sister in the "League of Legends" Three Sisters triad?',
+      answers: [
+        'Kalista',
+        'Katerina',
+        'Akali',
+        'Lissandra'
+      ],
+      correctAnswer: 'Lissandra'
+    },
+    {
+      question: 'Which "League of Legends" character leads the Winter Claw tribe?',
+      answers: [
+        'Nunu',
+        'Lissandra',
+        'Quinn',
+        'Sejuani'
+      ],
+      correctAnswer: 'Sejuani'
+    },
+    {
+      question: 'Which "League of Legends" champion does not come from the Void?',
+      answers: [
+        'Kha Zix',
+        'Cho Gath',
+        'Karthus',
+        'Vel koz'
+      ],
+      correctAnswer: 'Karthus'
+    },
+    {
+      question: 'In "League of Legends" Cassiopeia is half woman and half what creature?',
+      answers: [
+        'horse',
+        'lion',
+        'snake',
+        'fish'
+      ],
+      correctAnswer: 'snake'
+    },
+
   ],
   quizStarted: false,
   questionNumber: 0,
@@ -48,10 +129,8 @@ const store = {
 function welcomePageSource() {
   return `
   <div class='welcome'>
-    <h1>What Is Your Rank?</h1>
-    <h2>League of Legend Pop-Quiz!</h2>
-    <h2>League of Legend Pop-Quiz! Submit your summonername to start the quiz.</h2>
-    <img src="" alt="">//<!--add an imagae on the page-->
+    <h1>League of Legend Pop-Quiz!</h1>
+    <h2>Submit your summonername to start the quiz.</h2>
     <div class="ready-section">
     <label for="welcome-page">Your Summonername</label>
     <input type="text" name="welcome-page" class='quiz-welcome-page' placeholder="e.g. BestLeeSinNA">
@@ -61,33 +140,43 @@ function welcomePageSource() {
 `;
 }
 function generateQuestion(counter) {
-  return `<div class="equiz">
-<h2>${store.questions[counter].question}</h2>
-<form class="tomwallace">
-<input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[0]}">
-<label for="male">${store.questions[counter].answers[0]}</label><br>
-<input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[1]}">
-<label for="female">${store.questions[counter].answers[1]}</label><br>
-<input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[2]}">
-<label for="other">${store.questions[counter].answers[2]}</label>
-<input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[3]}">
-<label for="other">${store.questions[counter].answers[3]}</label>
-<button type="submit">Submit</button>
-</form>
+  return `
+  <div class="equiz">
+    <h2>${store.questions[counter].question}</h2><br>
+    
+    <form class="tomwallace">
+      <input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[0]}">
+      <label for="male">${store.questions[counter].answers[0]}</label><br>
+      
+      <input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[1]}">
+      <label for="female">${store.questions[counter].answers[1]}</label><br>
+      
+      <input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[2]}">
+      <label for="other">${store.questions[counter].answers[2]}</label><br>
+      
+      <input type="radio" id="answers" name="answer" value="${store.questions[counter].answers[3]}">
+      <label for="other">${store.questions[counter].answers[3]}</label><br>
+
+      <button type="submit">Submit</button>
+    </form>
 </div>`;
 }
+
 function returnCorrectAnswer() {
-  const correctTemplate = `<div class='correct-answer'>
-  <h1 class='right-or-wrong'>You Got It!</h1>
-  <button class='next-or-back'>Next Question</button>
+  const correctTemplate = `
+  <div class='correct-answer'>
+    <h1 class='right-or-wrong'>You Got It!</h1>
+    <button class='next-or-back'>Next Question</button>
   </div>
   `;
   return correctTemplate;
 }
+
 function returnThatsWrong() {
-  const wrongTemplate = `<div class='correct-answer'>
-  <h1 class='right-or-wrong'>That's not it!</h1>
-  <button class='next-or-back'>Next Question</button>
+  const wrongTemplate = `
+  <div class='correct-answer'>
+    <h1 class='right-or-wrong'>That's not it!</h1>
+    <button class='next-or-back'>Next Question</button>
   </div>
   `;
   return wrongTemplate;
@@ -137,20 +226,42 @@ function renderResults(answer) {
   $('.main').html(page);
   handleNextQuestion();
 }
+
 function renderAll(template) {
   let page = '';
   if (store.quizStarted === false) {
     page += welcomePageSource();
   }
-  if (store.quizStarted === true && store.questionNumber < 5) {
+  if (store.quizStarted === true && store.questionNumber < 10) {
     page += template;
   }
+  if (store.quizStarted === true && store.questionNumber === 10 ){
+    page += finalPageSource();
+  }
+
   $('.main').html(page);
 }
+
+function restartQuiz(){
+  $('.thankyou-page').on('click', 'restart-butt', function (){
+    startQuiz();
+  });
+}
+
+function finalPageSource(){
+  return `
+  <div class="thankyou-page">
+    <h1 class = "thankyou">Thank you For Taking The quiz!</h1> 
+    <h3 class="thankyou">Think you can do better? Try It Again!</h3>
+    <button class="restart-butt">Restart</button>
+  </div>`;
+}
+
 function main() {
   renderAll();
   startQuiz();
   handleAnswerChoice();
   handleNextQuestion();
+  restartQuiz()
 }
 $(main);
